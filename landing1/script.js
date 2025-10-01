4f3745ccd808346756cc087d6a08b81ba54429cc
kamilaacarvalhoo <kamilacarvalho9929@gmail.com>
2025-10-01 17:05:28 -0300

    coloquei fotos na floricultura

--- landing1/script.js
+++ landing1/script.js
@@ -1,16 +1,24 @@
-  document.querySelectorAll('a[href^="#"]').forEach(link => {
-      link.addEventListener('click', function(e){
-        e.preventDefault();
-        const alvo = document.querySelector(this.getAttribute('href'));
-        if(alvo){
-          window.scrollTo({ top: alvo.offsetTop - 60, behavior:"smooth" });
-        }
-      });
-    });
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
 
-    const form = document.querySelector('.contato form');
-    form.addEventListener('submit', e => {
-      e.preventDefault();
-      alert("Obrigado por entrar em contato! 🌸 Logo retornaremos.");
-      form.reset();
-    });
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      const offset = targetElement.offsetTop - 60;
+
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+const form = document.querySelector(".contato form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Obrigado por entrar em contato! 🌸 Logo retornaremos.");
+  form.reset();
+});
